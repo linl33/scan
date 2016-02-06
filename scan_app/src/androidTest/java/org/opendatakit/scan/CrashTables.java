bd10ac1d1da6a15430bc69eebca682080b5886a7
linl33 <linlee1997@gmail.com>
2016-02-06 13:28:44 -0800

    Add a test to start/kill Tables

---
+++ scan_app/src/androidTest/java/org/opendatakit/scan/CrashTables.java
@@ -0,0 +1,89 @@
+package org.opendatakit.scan;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.RemoteException;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CrashTables {
+  private static final int APP_START_TIMEOUT = 10000;
+  private static final int APP_INIT_TIMEOUT = 10000;
+  private static final int OBJ_WAIT_TIMEOUT = 1000;
+
+  private UiDevice mDevice;
+
+  @Before
+  public void setup() {
+    mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+  }
+
+  @Test
+  public void crashBy_fragmentNullPtr() throws IOException, RemoteException {
+    while (true) {
+      startApp(mDevice, "org.opendatakit.tables");
+
+      assertThat(
+          "Tables cannot be started or crashed",
+          mDevice.wait(Until.findObject(By.desc("Web View")), 10000) != null,
+          is(true));
+
+      closeApp(mDevice, "ODK Tables", "org.opendatakit.tables", 2);
+    }
+  }
+
+  private static void startApp(UiDevice mDevice, String pkgName) {
+    mDevice.pressHome(); //Start from home screen
+
+    //wait for package launcher
+    mDevice.wait(Until.hasObject(By.pkg(getLauncherPackageName()).depth(0)), APP_START_TIMEOUT);
+
+    //start app
+    Context context = InstrumentationRegistry.getContext();
+    final Intent intent = context.getPackageManager().getLaunchIntentForPackage(pkgName);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    context.startActivity(intent);
+
+    //wait for app to start
+    mDevice.wait(Until.hasObject(By.pkg(pkgName).depth(0)), APP_START_TIMEOUT);
+  }
+
+  /**
+   * WARNING:
+   * This might not work on all versions of Android
+   *
+   * appName is name of app displayed on device, for example "ODK Tables"
+   */
+  private static void closeApp(UiDevice mDevice, String appName, String pkgName, int method)
+      throws RemoteException, IOException {
+    if (method == 1) {
+      mDevice.pressRecentApps();
+      //swipe to kill app
+      mDevice.wait(Until.findObject(By.text(appName)), OBJ_WAIT_TIMEOUT)
+          .swipe(Direction.RIGHT, 1.0f);
+      mDevice.pressHome();
+    } else {
+      mDevice.executeShellCommand("am force-stop " + pkgName);
+    }
+  }
+
+  private static String getLauncherPackageName() {
+    // Create launcher Intent
+    final Intent intent = new Intent(Intent.ACTION_MAIN);
+    intent.addCategory(Intent.CATEGORY_HOME);
+
+    // Use PackageManager to get the launcher package name
+    PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+    ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+    return resolveInfo.activityInfo.packageName;
+  }
+}
